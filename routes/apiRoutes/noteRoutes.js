1e5e56255c626f5da4bed55de865090b0bb1525e
nverhulp <105401594+nverhulp@users.noreply.github.com>
2022-10-18 17:08:10 -0700

    created routes

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,16 @@
+const router = require("express").Router();
+const { notes } = require('../../db/db');
+const { createNewNote } = require('../../lib/notes');
+
+router.get('/notes', (req, res) => {
+    let saved = notes;
+    res.json(saved);
+});
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+    let note = createNewNote(req.body, notes);
+    res.json(note);
+})
+
+module.exports = router;
